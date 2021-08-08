818cc91f7f35fb2c9c144f98563c3b540c273bea
Siyanbil Orlan <siyanbil.orlan@gmail.com>
2021-08-08 23:55:09 +0700

    lesson-6

---
+++ task6_3.py
@@ -0,0 +1,34 @@
+# Реализовать базовый класс Worker (работник), в котором определить атрибуты: name, surname, position (должность),
+# income (доход). Последний атрибут должен быть защищенным и ссылаться на словарь, содержащий элементы: оклад и премия,
+# например, {"wage": wage, "bonus": bonus}. Создать класс Position (должность) на базе класса Worker. В классе Position
+# реализовать методы получения полного имени сотрудника (get_full_name) и дохода с учетом премии (get_total_income).
+# Проверить работу примера на реальных данных (создать экземпляры класса Position, передать данные,
+# проверить значения атрибутов, вызвать методы экземпляров).
+
+class Worker:
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+    def __init__(self, name, surname, position, wage, bonus):
+        super().__init__(name, surname, position, wage, bonus)
+
+    def get_full_name(self):
+        return f"Полное имя сотрудника: {self.surname} {self.name}"
+
+    def get_total_income(self):
+        result = self._income["wage"] + self._income["bonus"]
+        return f"Доход: {result}"
+
+
+p = Position("Orlan", "Siyanbil", "librarian", 1000, 100)
+print(p.name)
+print(p.surname)
+print(p.position)
+print(p._income)
+print(p.get_full_name())
+print(p.get_total_income())
